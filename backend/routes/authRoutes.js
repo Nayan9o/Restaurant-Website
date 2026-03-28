7f32b99074bdbb2e85e58f0298bee1d005df6e11
Nayan9o <nayanmeshram9o9@gmail.com>
2026-03-28 21:04:10 +0530

    add login post route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { loginUser, registerUser } from "../controllers/authController.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/register",registerUser)
+authRoutes.post("/login",loginUser)
 
 export default authRoutes
